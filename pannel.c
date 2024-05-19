6bc4071cb7a6c8c5af997e811dfe2c9f9f77ffb1
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2024-05-19 17:06:37 +0000

    Pannel base

--- pannel.c
+++ pannel.c
@@ -1,2 +1,40 @@
-#include "panel.h"
+#include "pannel.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+Pannel *
+PannelCreate(
+        int16_t x,
+        int16_t y,
+        uint16_t w,
+        uint16_t h
+        )
+{
+    Pannel *ret = malloc(sizeof(Pannel ));
+    if(ret)
+    {
+        ret->x = x;
+        ret->y = y;
+        ret->w = w;
+        ret->h = h;
+    }
+    return ret;
+}
+
+void
+PannelMap(
+        Pannel *pannel
+        )
+{   
+}
+
+void
+PannelUnmap(
+        Pannel * pannel
+        )
+{
+
+}
 
