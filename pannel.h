86b89cf12a6670dd0f9fadcaece242850ed5b14d
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2024-05-19 17:06:50 +0000

    Pannel base (#64)

--- pannel.h
+++ pannel.h
@@ -1,4 +1,45 @@
 #ifndef PANNEL_H_
 #define PANNEL_H_
 
+#include "XCB-TRL/xcb_trl.h"
+#include "util.h"
+#include <stdint.h>
+
+typedef struct Pannel Pannel;
+typedef struct PannelWidget PannelWidget;
+
+struct Pannel
+{
+    int16_t x;
+    int16_t y;
+    uint16_t w;
+    uint16_t h;
+
+    /* pix */
+    XCBWindow win;
+    PannelWidget **wigets;
+};
+
+struct PannelWidget
+{
+};
+
+
+Pannel *
+PannelCreate(
+        i16 x,
+        i16 y,
+        u16 w,
+        u16 h
+        );
+void
+PannelMap(
+        Pannel *pannel
+        );
+void
+PannelUnmap(
+        Pannel * pannel
+        );
+
+
 #endif
