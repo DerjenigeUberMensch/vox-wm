c865da215068aac09635d7244a0e36390a80bbf1
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2024-05-20 23:46:15 +0000

    Cleanup + cleanup clientmessage() in events.c (#89)

--- dwm.h
+++ dwm.h
@@ -323,10 +323,6 @@ struct WM
 
 /* Handles the main(int argc, char **argv) arguments. */
 void argcvhandler(int argc, char *argv[]);
-/* Applies size restrictions to the specified values.
- * NOTE: Cannot handle any NULL values.
-*/
-void applysizechecks(Client *c, int32_t *x, int32_t *y, int32_t *width, int32_t *height, int32_t *border_width);
 /* Applies the gravity shifts specified by the gravity onto the x and y coordinates.
 */
 void applygravity(const uint32_t gravity, int16_t *x, int16_t *y, const uint16_t width, const uint16_t height, const uint16_t border_width);
