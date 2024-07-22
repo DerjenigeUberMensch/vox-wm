81f88bb56292b95127577892be628c8aed93abed
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2024-07-23 00:32:04 +0800

    Removed x.h Get() functions

--- include/x.h
+++ include/x.h
@@ -23,24 +23,6 @@ uint8_t checksticky(int64_t x);
 char *getnamefromreply(XCBWindowProperty *namerep);
 /* Gets the icon property from the specified XCBWindowProperty. */
 uint32_t *geticonprop(XCBWindowProperty *iconreply);
-
-
-/* Getters */
-XCBCookie GetTransientCookie(XCBDisplay *display, XCBWindow window);
-XCBCookie GetWindowStateCookie(XCBDisplay *display, XCBWindow window);
-XCBCookie GetWindowTypeCookie(XCBDisplay *display, XCBWindow window);
-XCBCookie GetSizeHintsCookie(XCBDisplay *display, XCBWindow window);
-XCBCookie GetWMHintsCookie(XCBDisplay *display, XCBWindow window);
-XCBCookie GetWMClassCookie(XCBDisplay *display, XCBWindow window);
-XCBCookie GetWMProtocolCookie(XCBDisplay *display, XCBWindow window);
-XCBCookie GetStrutCookie(XCBDisplay *display, XCBWindow window);
-XCBCookie GetStrutpCookie(XCBDisplay *display, XCBWindow window);
-XCBCookie GetNetWMNameCookie(XCBDisplay *display, XCBWindow window);
-XCBCookie GetWMNameCookie(XCBDisplay *display, XCBWindow window);
-XCBCookie GetPidCookie(XCBDisplay *display, XCBWindow window);
-XCBCookie GetIconCookie(XCBDisplay *display, XCBWindow window);
-XCBCookie GetMotifHintsCookie(XCBDisplay *display, XCBWindow window);
-
 char *GetAtomNameQuick(XCBDisplay *display, XCBAtom atom);
 
 #endif
