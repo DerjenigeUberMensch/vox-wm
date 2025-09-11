d10ebc8ef9ed0a15dad7661c77c27dba5fbc86e8
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2025-09-11 17:25:50 -0500

    License clarification util.h
    Signed-off-by: Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>

--- tools/util.h
+++ tools/util.h
@@ -1,4 +1,29 @@
-/* See LICENSE file for copyright and license details. */
+/* MIT License
+ *
+ * Copyright (c) 2024- Joseph
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Unless otherwise specified, everything in this file is under the MIT License.
+ */
+
 #ifndef __UTIL_H__42
 #define __UTIL_H__42
 
@@ -437,6 +462,8 @@ union ARGB
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
+ * The below macro 'MERGE_SORT_LINKED_LIST' falls under the above license.
  */
 #define MERGE_SORT_LINKED_LIST(TYPE, CMP_FUNC, HEAD, TAIL, NEXT, PREV, IS_DOUBLE, IS_CIRCULAR)\
                         do                                                              \
