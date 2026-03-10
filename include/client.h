239f3ae8b3c13ec613a7620a23994aad36edf69c
DerjenigeUberMensch <119084558+DerjenigeUberMensch@users.noreply.github.com>
2026-03-09 19:54:10 -0500

    Fixed naming for manage() args (#621)

--- include/client.h
+++ include/client.h
@@ -278,7 +278,7 @@ void NonNullAll managereplies(XCBCookie requests[ManageClientLAST], void *replie
  * RETURN: Client * on Success.
  * RETURN: NULL on Failure.
  */
-Client *NonNullAll manage(XCBWindow window, bool ignore_unmapped, void *replies[ManageClientLAST]);
+Client *NonNullAll manage(XCBWindow window, bool allow_unmapped_window, void *replies[ManageClientLAST]);
 /* Maximizes a client if unmaxed, Sets flag.
  */
 void NonNull maximize(Client *c);
@@ -630,7 +630,7 @@ uint16_t NonNull WIDTH(Client *c);
 uint16_t NonNull HEIGHT(Client *c);
 
 /* manage */
-uint32_t CANMANAGE(XCBWindow win, bool ignore_unmapped, XCBGetWindowAttributes *waattributes, XCBWindowProperty *wastate);
+uint32_t CANMANAGE(XCBWindow win, bool allow_unmapped_window, XCBGetWindowAttributes *waattributes, XCBWindowProperty *wastate);
 
 
 
