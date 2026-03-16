0f8921ddfae6bd81acd2c6cfe7d2ea7b74297d52
Joseph <119084558+DerjenigeUberMensch@users.noreply.github.com>
2026-03-16 09:27:05 +0800

    Fixed versioning in generate_release.sh

--- generate_release.sh
+++ generate_release.sh
@@ -1,11 +1,11 @@
 mkdir -p Release
 
 make clean
-make -j$(nproc) release VERSION=v$1
+make -j$(nproc) release
 mv ./bin/release/vox-wm Release/vox-wmX86-64
 cp Release/vox-wmX86-64 Release/vox-wm
 make clean
-make -j$(nproc) debug VERSION=v$1
+make -j$(nproc) debug
 mv ./bin/debug/vox-wm Release/vox-wmX86-64-debug
 
 echo "Running Tests..."
